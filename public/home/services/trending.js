a1ce44095ca3d47b667f31d95cdac85ae6f77f2e
Ansel Rosenberg <ansel01@gmail.com>
2015-10-18 17:37:07 -0700

    Update trending players

--- public/home/services/trending.js
+++ public/home/services/trending.js
@@ -18,13 +18,12 @@
     // Ensures trending players have been retreived
     function getPlayers() {
       var def = $q.defer();
-      if (false) {
+      if (instance.players.length) {
         def.resolve()
       } else {
-        $http.get('http://localhost:5000/trending')
-        // $http.get('http://sports-stats-pro.herokuapp.com/trending')
+        $http.get('http://sports-stats-pro.herokuapp.com/trending')
         .then(function(res) {
-          instance.players = res.data;
+          instance.players = _.first(res.data, 15);
           def.resolve();
         }, function(err) {
           def.reject(err);
